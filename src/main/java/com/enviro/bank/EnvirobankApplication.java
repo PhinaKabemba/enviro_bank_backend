5ad0af369d430ca6336e68f3a97a563b6d0a2e01
Phina Kabemba <kabembaphina@gmail.com>
2022-03-31 23:26:47 +0200

    Initial commit for enviro bank back end

---
+++ src/main/java/com/enviro/bank/EnvirobankApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvirobankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvirobankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enviro/bank/EnvirobankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvirobankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
